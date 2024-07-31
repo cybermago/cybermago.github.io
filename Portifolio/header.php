58a9140c332043961bef0471300ea3ed5a20d9aa
cybermago <cybermago@outlook.com>
2024-07-31 16:26:37 -0300

    Initial Commit

---
+++ Portifolio/header.php
@@ -0,0 +1,39 @@
+
+
+<nav class="navbar navbar-expand-lg text-uppercase fixed-top" id="mainNav" style="background-color: #1a252f;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#page-top">Jonatã Mendes</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">Sobre min</a></li>
+        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#formacao">Formação</a></li>
+        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#linguagens">Linguagens</a></li>
+        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contato</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        <!-- Masthead-->
+        <header class="masthead bg-primary text-white text-center" style="height: 100%;">
+            <div class="container d-flex align-items-center flex-column" style="height: 100%;">
+                <!-- Masthead Avatar Image-->
+                <img class="masthead-avatar mb-5" src="assets/imgs/foto-perfil.jpg" style="border-radius: 50%;" alt="..." />
+                <!-- Masthead Heading-->
+                <h1 class="masthead-heading text-uppercase mb-0">Cientista da Computação</h1>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0">Desenvolvedor Backend - Designer Gráfico - VideoMaker</p>
+            </div>
+        </header>
